767356392b60bb18ad1131b12dd36bddec609609
jensen <563299377@qq.com>
2019-03-21 22:25:55 +0800

    origin

--- train.py
+++ train.py
@@ -2,7 +2,8 @@
 import os
 import sys
 import h5py
-import matplotlib.pyplot as plt
+
+
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -13,13 +14,13 @@ sys.path.append(os.path.join('models'))
 TRAIN_FILES_DIR=os.path.join(BASE_DIR,'data/modelnet40_ply_hdf5_2048/train_files.txt')
 
 import providerUtil
+import points2pcd
 
 #获得数据
 TRAIN_FILES=providerUtil.getDataFiles(os.path.join(TRAIN_FILES_DIR))
-FileHandle=h5py.File(TRAIN_FILES[0],'r')
-p=FileHandle['data'][0]
-
-#绘制空间形状
+FileHandle=h5py.File(TRAIN_FILES[3],'r')
+p=FileHandle['data'][203]
+points2pcd.points2pcd(p)
 
 
 
